67cb6cc4eb383b52b38b54fe938596b394b0a9ba
Liza <liza123.97@mail.ru>
2019-05-05 14:51:28 +0300

    commit

---
+++ my-app/build/precache-manifest.71c9070f15517c76e987451b2cdfb97f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce499b08307ead20c837e3ef0244031",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6e9feb28e07ef89f4a8",
+    "url": "/static/css/main.3403440e.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "f6e9feb28e07ef89f4a8",
+    "url": "/static/js/main.5cc79767.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7ffe7748f6ad455de2de60c62573796",
+    "url": "/static/media/cup_1280.b7ffe774.svg"
+  },
+  {
+    "revision": "32b13a461f2933dc0d4a3d2a6ab6b356",
+    "url": "/static/media/main_1920.32b13a46.svg"
+  }
+]);
